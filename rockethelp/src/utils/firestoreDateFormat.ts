6ef88441e139a4abe4cf3848d2e07be27e648dce
paloma.souza <paloma.souza@ambevtech.com.br>
2022-07-21 22:45:19 -0300

    fix - data  i18n gradle

--- rockethelp/src/utils/firestoreDateFormat.ts
+++ rockethelp/src/utils/firestoreDateFormat.ts
@@ -1,12 +1,15 @@
-import { FirebaseFirestoreTypes} from '@react-native-firebase/firestore'
+import { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
 
 export function dateFormat(timestamp: FirebaseFirestoreTypes.Timestamp) {
-    if (timestamp) {
-        const date = new Date(timestamp.toDate());
+  if (timestamp) {
+    const date = new Date(timestamp.toDate());
 
-        const day = date.toLocaleDateString('pt-BR')
-        const hour = date.toLocaleTimeString('pt-BR')
-        
-        return `${day} às ${hour}`
-    }
+    const day = date.toLocaleDateString('pt-BR');
+    const hour = date.toLocaleTimeString('pt-BR', {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+    console.log(date);
+    return `${day} às ${hour}`;
+  }
 }
